4eb4dae82aa316fc7eeb074d3e448df2503b1e67
Nico <nico_nobile@hotmail.com>
2017-05-20 00:06:22 -0300

    Agrego la creacion de las tablas de cliente, facturacion cliente y registro de viaje

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -49,3 +49,43 @@ create table [GDD].RolporUsuario(
  PRIMARY KEY (IdUsuario, IdRol)
 )
 go
+
+/*Cliente */
+create table [GDD].Cliente(
+IdCliente int identity(1,1) primary key,
+Nombre varchar(50) not null,
+Apellido varchar(50) not null,
+Mail varchar(50),
+Telefono numeric(18,0) unique not null,
+Direccion varchar(255) not null,
+Codigo_postal int not null,
+Fecha_de_nacimiento datetime not null,
+DNI numeric(18,0) unique not null,
+IdUsuario int not null references [GDD].Usuario,
+)
+go
+
+/*Facturacion cliente*/
+create table [GDD].FacturacionCliente(
+IdFactura int identity(1,1) primary key,
+Fecha_inicio_de_factura datetime /*notnull*/,                        /*En la tabla maestra las fechas estan en null, por las dudas comento el not null*/
+Fecha_fin_de_factura datetime /*notnull*/,
+IdCliente int not null references [GDD].Cliente,
+Importe_total_de_factura numeric(18,2) not null,
+Viajes_facturados numeric(18,0) not null,               /*MUCHACHOS, CREO QUE ACA LA PIFIAMOS, QUE VIAJES FACTURADOS DEBERIA SER LA PK DE REGISTRO DE VIAJE, PORQUE ESTO CREO QUE VENDRIA A SER EL DETALLE*/
+)
+go
+
+/*Registro de viaje*/
+create table [GDD].RegistroViaje(
+IdViaje int identity(1,1) primary key,
+/*IdChofer int not null references [GDD].Chofer,*/                          /*Descomentar una vez creada la tabla chofer*/
+Automovil int not null,                                                     /*No estoy seguro*/
+IdFactura int not null references [GDD].FacturacionCliente,
+/*IdTurno int not null references [GDD].Turno,*/                            /*Descomentar una vez creada la tabla turno*/
+Cantidad_de_km_viaje numeric(18,0) not null, 
+Fecha_hora_inicio_de_viaje datetime not null,
+Fecha_hora_fin_de_viaje datetime not null,              /*ACA PODRIAMOS SEPARAR LA FECHA DE LA HORA (SOLO SUGERENCIA)*/
+IdCliente int not null references [GDD].Cliente,
+)
+go
