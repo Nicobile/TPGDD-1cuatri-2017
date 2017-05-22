0cb4d4daf31fae1ea494c84148799e97a5544822
Amoroso Lucas <Amoroso Lucas>
2017-05-21 21:42:50 -0300

    migracion

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -83,7 +83,7 @@ CREATE TABLE [PUSH_IT_TO_THE_LIMIT].RolporFunciones(
 /*Usuario FALTA CONTRASEÑA DEFAULT QUE ES W23A CREO QUE EN SHA256*/
 CREATE TABLE [PUSH_IT_TO_THE_LIMIT].Usuario(
 	[usuario_id] INT IDENTITY(1,1) PRIMARY KEY,
-	[usuario_name] VARCHAR(50) UNIQUE NOT NULL,
+	[usuario_name] VARCHAR(255) UNIQUE NOT NULL,
 	[usuario_password] VARCHAR(50) NOT NULL,
 	[usuario_habilitado] [BIT] NOT NULL DEFAULT 1,
 	[usuario_intentos] [TINYINT] DEFAULT 0,
@@ -120,6 +120,7 @@ CREATE TABLE [PUSH_IT_TO_THE_LIMIT].Factura(
 	[factura_id] INT IDENTITY(1,1) PRIMARY KEY,
 	[factura_fecha_inicio] DATETIME , /*En la tabla maestra las fechas estan en null, por las dudas comento el not null*/
 	[factura_fecha_fin] DATETIME ,
+	[factura_numero] numeric(18,0) unique not null,
 	[cliente_id] INT NOT NULL REFERENCES [PUSH_IT_TO_THE_LIMIT].Cliente,
 	[factura_importe_total] NUMERIC(18,2) ,
 	)
@@ -158,6 +159,7 @@ CREATE TABLE [PUSH_IT_TO_THE_LIMIT].[Turno](
 CREATE TABLE [PUSH_IT_TO_THE_LIMIT].[RendicionViaje](
 	[rendicion_id] INT IDENTITY(1,1) PRIMARY KEY,
 	[rendicion_fecha] DATETIME NOT NULL,
+	[rendicion_numero] numeric(18,0) not null,
 	[chofer_dni] numeric(18,0) NOT NULL ,--REFERENCES [PUSH_IT_TO_THE_LIMIT].[Chofer],					
 	[turno_id] int NOT NULL ,--REFERENCES [PUSH_IT_TO_THE_LIMIT].[Turno],					
 	[rendicion_importe_total] NUMERIC(18,2) NOT NULL,    
@@ -189,8 +191,10 @@ create table [PUSH_IT_TO_THE_LIMIT].RegistroViaje(
 	[factura_id] INT NOT NULL ,--references [PUSH_IT_TO_THE_LIMIT].Factura,
 	[turno_id] INT NOT NULL ,--references [PUSH_IT_TO_THE_LIMIT].Turno,                            
 	[viaje_cantidad_km] NUMERIC(18,0) NOT NULL, 
-	[viaje_fecha_hora_inicio] DATETIME NOT NULL,
-	[viaje_fecha_hora_fin] DATETIME NOT NULL,              /*ACA PODRIAMOS SEPARAR LA FECHA DE LA HORA (SOLO SUGERENCIA)*/
+	[viaje_rendicion] numeric(10,0),
+	[viaje_fecha] DATETIME NOT NULL,
+	[viaje_hora_inicio] time not null,
+	[viaje_hora_fin] TIME NOT NULL,              /*ACA PODRIAMOS SEPARAR LA FECHA DE LA HORA (SOLO SUGERENCIA)*/
 	[cliente_id] INT NOT NULL ,--references [PUSH_IT_TO_THE_LIMIT].Cliente,
 )
 
@@ -238,35 +242,156 @@ ALTER TABLE [PUSH_IT_TO_THE_LIMIT].[AutoporTurno] ADD CONSTRAINT AutoporTurno_Au
 ALTER TABLE [PUSH_IT_TO_THE_LIMIT].[AutoporTurno] ADD CONSTRAINT AutoporTurno_Turno FOREIGN KEY (Turno_id) REFERENCES [PUSH_IT_TO_THE_LIMIT].[Turno](turno_id)
 
 
+/*Migracion*/
+
+/*Rol*/
+insert into [PUSH_IT_TO_THE_LIMIT].Rol (rol_nombre) values
+('Administrativo'), 
+('Chofer'), 
+('Cliente');
+
+
+
+/*Funcionalidad*/
+
+
+
+insert into [PUSH_IT_TO_THE_LIMIT].Funcionalidad (funcionalidad_descripcion) values
+('ABM de Rol'),
+('Registro de usuarios'),
+('ABM de Clientes'),
+('ABM de Automoviles'),
+('ABM de turnos'),
+('ABM de choferes'),
+('Registro de viajes'),
+('Rendicion de viajes'),
+('Facturacion de clientes'),
+('Listado estadistico');
+
+
+/*RolXFuncionalidad*/
+insert into [PUSH_IT_TO_THE_LIMIT].RolporFunciones (rol_id, funcionalidad_id) values
+(1,1), (1,2), (1,3), (1,4),(1,5),(1,6),(1,7),(2,8),(3,9),(1,10),(2,10),(3,10);/* no estoy muy seguro de esta*/
+
+
+
+/*Cliente*/
+insert into [PUSH_IT_TO_THE_LIMIT].Cliente (cliente_nombre, cliente_apellido,cliente_dni, cliente_telefono,cliente_direccion,cliente_mail,cliente_fecha_nacimiento, usuario_id)
+select distinct m.Cliente_Nombre, m.Cliente_Apellido, m.Cliente_Dni, m.Cliente_Telefono, m.Cliente_Direccion, m.Cliente_Mail, m.Cliente_Fecha_Nac, u.usuario_ID
+from gd_esquema.Maestra m, [PUSH_IT_TO_THE_LIMIT].Usuario u
+where  cast( m.Cliente_Dni as varchar(255)) = u.usuario_name
+order by usuario_ID
+
+
+/*Usuarios*/
+
+
+/*Usuario Administrador*/
+insert into [PUSH_IT_TO_THE_LIMIT].Usuario (usuario_name, usuario_password) values
+('admin',HASHBYTES('SHA2_256','w23e'))
+
+/*Usuarios choferes*/
+insert into [PUSH_IT_TO_THE_LIMIT].Usuario (usuario_name, usuario_password)
+select distinct cast(Chofer_Dni as varchar(255)), HASHBYTES('SHA2_256',cast(Chofer_Dni as varchar(255)))
+from gd_esquema.Maestra
+where Chofer_Dni is not null
+order by cast(Chofer_Dni as varchar(255))
+
+
+/*usuarios clientes*/
+insert into [PUSH_IT_TO_THE_LIMIT].Usuario (usuario_name, usuario_password)
+select distinct cast(Cliente_Dni as varchar(255)), HASHBYTES('SHA2_256',cast(Cliente_Dni as varchar(255)))
+from gd_esquema.Maestra
+where Cliente_Dni is not null
+order by cast(Cliente_Dni as varchar(255))
+
+
+						/*RolporUsuario*/
+/*clienteporusuario*/
+insert into [PUSH_IT_TO_THE_LIMIT].RolporUsuario( usuario_Id, rol_id)
+select distinct u.usuario_ID, r.rol_ID
+from [PUSH_IT_TO_THE_LIMIT].Usuario u, [PUSH_IT_TO_THE_LIMIT].Cliente c, [PUSH_IT_TO_THE_LIMIT].Rol r
+where u.usuario_name = cast(c.cliente_dni as varchar(255))
+and r.rol_nombre = ('Cliente')
+
+/*choferporusuario*/
+insert into [PUSH_IT_TO_THE_LIMIT].RolporUsuario( usuario_Id, rol_id)
+select distinct u.usuario_ID, r.rol_ID
+from [PUSH_IT_TO_THE_LIMIT].Usuario u, [PUSH_IT_TO_THE_LIMIT].Chofer c, [PUSH_IT_TO_THE_LIMIT].Rol r
+where u.usuario_name = cast(c.chofer_dni as varchar(255))
+and r.rol_nombre = ('Chofer')
+
+/*RendicionViaje*/  
+insert into [PUSH_IT_TO_THE_LIMIT].RendicionViaje( chofer_dni, rendicion_fecha, rendicion_importe_total, rendicion_numero, turno_id)
+select distinct c.chofer_direccion, m.Rendicion_Fecha, sum(m.Rendicion_Importe), m.Rendicion_Nro, t.turno_id
+from gd_esquema.Maestra m,  [PUSH_IT_TO_THE_LIMIT].Chofer c, [PUSH_IT_TO_THE_LIMIT].Turno t
+where m.Rendicion_Fecha is not null
+and m.Chofer_Dni = c.chofer_dni
+and m.Turno_Hora_Inicio = t.turno_hora_inicio
+group by Rendicion_Nro, c.chofer_dni, t.turno_id, m.Rendicion_Fecha
+order by Rendicion_Nro
+
+
+
+/*Auto*/
+insert into [PUSH_IT_TO_THE_LIMIT].Auto (auto_licencia, auto_marca, auto_modelo, auto_patente, auto_rodado, chofer_dni)
+select distinct m.Auto_Licencia, m.Auto_Marca, m.Auto_Modelo, m.Auto_Patente, m.Auto_Rodado, c.chofer_dni
+from gd_esquema.Maestra m, [PUSH_IT_TO_THE_LIMIT].Chofer c, [PUSH_IT_TO_THE_LIMIT].Turno t
+where m.Auto_Patente is not null
+and m.Chofer_Dni = c.chofer_dni
+
+/*AutosporTurno*/
+insert into [PUSH_IT_TO_THE_LIMIT].AutoporTurno (auto_patente, turno_id)
+select distinct  a.auto_patente, t.turno_id
+from gd_esquema.Maestra m, [PUSH_IT_TO_THE_LIMIT].[Auto] a, [PUSH_IT_TO_THE_LIMIT].Turno t
+where m.Auto_Patente = a.auto_patente
+and m.Turno_Hora_Inicio = t.turno_hora_inicio
+
+
+/*Turno*/
+
+insert into [PUSH_IT_TO_THE_LIMIT].Turno(turno_descripcion, turno_hora_fin, turno_hora_inicio, turno_precio_base, turno_valor_kilometro)
+select distinct Turno_Descripcion, Turno_Hora_Fin, Turno_Hora_Inicio, Turno_Precio_Base, Turno_Valor_Kilometro
+from gd_esquema.Maestra
+order by Turno_Hora_Inicio
+
+
+
+/*Factura*/
+insert into [PUSH_IT_TO_THE_LIMIT].Factura (cliente_id, factura_fecha_inicio, factura_fecha_fin, factura_numero, factura_importe_total)
+select distinct c.cliente_id, m.Factura_Fecha_Inicio, m.Factura_Fecha_Fin, m.Factura_Nro, ( select sum(m2.Turno_Precio_Base + ( m2.Viaje_Cant_Kilometros * m2.Turno_Valor_Kilometro))   
+																							 from gd_esquema.Maestra m2
+																							where m2.Factura_Nro = m.Factura_Nro)
+from  gd_esquema.Maestra m,[PUSH_IT_TO_THE_LIMIT].Cliente c
+where Factura_Nro is not null
+and c.cliente_dni = m.Cliente_Dni
+order by Factura_Nro
+
+/*Chofer*/
+
+insert into [PUSH_IT_TO_THE_LIMIT].Chofer(chofer_nombre, chofer_apellido,chofer_dni, chofer_telefono,chofer_direccion,chofer_mail,chofer_fecha_nacimiento, usuario_id)
+select distinct m.chofer_Nombre, m.chofer_Apellido, m.chofer_dni, m.chofer_Telefono, m.chofer_Direccion, m.chofer_Mail, m.chofer_Fecha_Nac, u.usuario_ID
+from gd_esquema.Maestra m, [PUSH_IT_TO_THE_LIMIT].Usuario u
+where  cast( m.chofer_dni as varchar(255)) = u.usuario_name
+order by usuario_ID
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*RegistroViaje*/
+insert into [PUSH_IT_TO_THE_LIMIT].RegistroViaje (viaje_automovil, chofer_dni, cliente_id, viaje_rendicion, turno_id, viaje_cantidad_km, viaje_fecha, factura_id)
+select distinct  a.auto_patente, ch.chofer_dni, cl.cliente_id, r.rendicion_id, t.turno_id, m.Viaje_Cant_Kilometros, m.Viaje_Fecha, f.factura_id
+from [PUSH_IT_TO_THE_LIMIT].[Auto] a, [PUSH_IT_TO_THE_LIMIT].Chofer ch, [PUSH_IT_TO_THE_LIMIT].Cliente cl, [PUSH_IT_TO_THE_LIMIT].RendicionViaje r, [PUSH_IT_TO_THE_LIMIT].Turno t, gd_esquema.Maestra m, gd_esquema.Maestra m2, [PUSH_IT_TO_THE_LIMIT].Factura f
+where m.Viaje_Cant_Kilometros is not null
+and m.Auto_Patente = a.auto_patente
+and m.Chofer_Dni = ch.chofer_dni
+and m.Cliente_Dni = cl.cliente_dni
+and m.Rendicion_Nro = r.rendicion_numero
+and m.Turno_Hora_Inicio = t.turno_hora_inicio
+and m.Chofer_Dni = m2.Chofer_Dni
+and m.Cliente_Dni = m2.Cliente_Dni
+and m.Viaje_Fecha = m2.Viaje_Fecha
+and m2.Factura_Nro = f.factura_numero
+order by m.Viaje_Fecha
 
 
