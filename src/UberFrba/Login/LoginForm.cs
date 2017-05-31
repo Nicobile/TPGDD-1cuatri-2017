dfac63c24cea23ae5a065730b8a4224e3c6737f1
Nico <nico_nobile@hotmail.com>
2017-05-31 15:01:04 -0300

    Correcciones en el login y se agrega el drop del procedure de choferes

--- src/UberFrba/Login/LoginForm.cs
+++ src/UberFrba/Login/LoginForm.cs
@@ -43,7 +43,7 @@ namespace UberFrba.Login
 
 
             // Nos fijamos si el usuario y contraseña existen y esta habilitado
-            String query = "SELECT * FROM NET_A_CERO.Usuarios WHERE usr_usuario = @username AND usr_password = @password AND usr_activo = 1";
+            String query = "SELECT * FROM PUSH_IT_TO_THE_LIMIT.Usuario WHERE usuario_name = @username AND usuario_password = @password AND usuario_habilitado = 1";
 
             String usuario = this.textBoxUsuario.Text;
             // valida contraseña encriptada
@@ -58,24 +58,24 @@ namespace UberFrba.Login
             //Chequea el ingreso
             if (QueryHelper.Instance.readFrom(reader))
             {
-                MessageBox.Show("Bienvenido " + reader["usr_usuario"] + "!");
+                MessageBox.Show("Bienvenido " + reader["usuario_name"] + "!");
 
-                UsuarioSesion.Usuario.nombre = (String)reader["usr_usuario"];
-                UsuarioSesion.Usuario.id = (Int32)reader["usr_id"];
+                UsuarioSesion.Usuario.nombre = (String)reader["usuario_name"];
+                UsuarioSesion.Usuario.id = (Int32)reader["usuario_id"];
 
                 // Usuario logueado correctamente (intentos fallidos = 0)
                 parametros.Clear();
                 parametros.Add(new SqlParameter("@username", usuario));
-                String clearIntentosFallidos = "UPDATE NET_A_CERO.Usuarios SET usr_intentos = 0 WHERE usr_usuario = @username";
+                String clearIntentosFallidos = "PUSH_IT_TO_THE_LIMIT.Usuario SET usuario_intentos = 0 WHERE usuario_name = @username";
                 QueryBuilder.Instance.build(clearIntentosFallidos, parametros).ExecuteNonQuery();
 
                 // Se fija si es el primer inicio de sesion del usuario
                 parametros.Clear();
                 parametros.Add(new SqlParameter("@username", usuario));
-                String sesion = "SELECT usr_password FROM NET_A_CERO.Usuarios WHERE usr_usuario = @username"; 
+                String sesion = "SELECT usuario_password FROM PUSH_IT_TO_THE_LIMIT.Usuario WHERE usuario_name = @username"; 
                 String primerInicio = (String)QueryBuilder.Instance.build(sesion, parametros).ExecuteScalar();
                 //El primer inicio la contraseña es fija (OK)
-                if (primerInicio == "565339bc4d33d72817b583024112eb7f5cdf3e5eef0252d6ec1b9c9a94e12bb3") 
+                if (primerInicio == "565339bc4d33d72817b583024112eb7f5cdf3e5eef0252d6ec1b9c9a94e12bb3") //  QUEDA PENDIENTE PONER ACA LA CONTRASEÑA QUE VIENE POR DEFECTO EN LA BASE!!!!!!!!!!!
                 {
                     this.Hide();
                     new CambiarContrasena().ShowDialog();
@@ -85,7 +85,7 @@ namespace UberFrba.Login
                 parametros.Clear();
                 parametros.Add(new SqlParameter("@username", usuario));
 
-                String consultaRoles = "SELECT COUNT(rol_id) FROM NET_A_CERO.Usuarios_x_Rol WHERE (SELECT usr_id FROM NET_A_CERO.Usuarios WHERE usr_usuario = @username) = usr_id";
+                String consultaRoles = "SELECT COUNT(rol_id) PUSH_IT_TO_THE_LIMIT.RolporUsuario WHERE (SELECT usuario_id FROM PUSH_IT_TO_THE_LIMIT.Usuario WHERE usuario_name = @username) = usuario_id";
                 int cantidadDeRoles = (int)QueryBuilder.Instance.build(consultaRoles, parametros).ExecuteScalar();
 
                 if(cantidadDeRoles > 1)
@@ -98,7 +98,7 @@ namespace UberFrba.Login
                 {
                     parametros.Clear();
                     parametros.Add(new SqlParameter("@username", usuario));
-                    String rolDeUsuario = "SELECT r.rol_nombre FROM NET_A_CERO.Roles r, NET_A_CERO.Usuarios_x_Rol ru, NET_A_CERO.Usuarios u WHERE r.rol_id = ru.rol_id AND ru.usr_id = u.usr_id AND u.usr_usuario = @username";
+                    String rolDeUsuario = "SELECT r.rol_nombre FROM PUSH_IT_TO_THE_LIMIT.Rol r, PUSH_IT_TO_THE_LIMIT.RolporUsuario ru, PUSH_IT_TO_THE_LIMIT.Usuario u WHERE r.rol_id = ru.rol_id AND ru.usuario_id = u.usuario_id AND u.usuario_name = @username";
                     String rolUser = (String)QueryBuilder.Instance.build(rolDeUsuario, parametros).ExecuteScalar();
 
                     UsuarioSesion.Usuario.rol = rolUser;
@@ -121,7 +121,7 @@ namespace UberFrba.Login
                 // Se fija si el usuario era correcto
                 parametros.Clear();
                 parametros.Add(new SqlParameter("@username", usuario));
-                String buscaUsuario = "SELECT * FROM NET_A_CERO.Usuarios WHERE usr_usuario = @username";
+                String buscaUsuario = "SELECT * FROM PUSH_IT_TO_THE_LIMIT.Usuario WHERE usuario_name = @username";
                 SqlDataReader lector = QueryBuilder.Instance.build(buscaUsuario, parametros).ExecuteReader();
 
                 if (lector.Read())
@@ -131,34 +131,34 @@ namespace UberFrba.Login
                     parametros.Clear();
                     parametros.Add(new SqlParameter("@username", usuario));
                     parametros.Add(new SqlParameter("@password", contraseña));
-                    String estaDeshabilitado = "SELECT * FROM NET_A_CERO.Usuarios WHERE usr_usuario = @username AND usr_activo = 0";
+                    String estaDeshabilitado = "SELECT * FROM PUSH_IT_TO_THE_LIMIT.Usuario WHERE usuario_name = @username AND usuario_habilitado = 0";
 
                     SqlDataReader userDeshabilitado = QueryBuilder.Instance.build(estaDeshabilitado, parametros).ExecuteReader();
 
                     if (userDeshabilitado.Read())
                     {
-                        MessageBox.Show("El usuario esta deshabilitado");
+                        MessageBox.Show("El usuario no esta habilitado");
                         return;
                     }
 
                     // Suma un fallido
                     parametros.Clear();
                     parametros.Add(new SqlParameter("@username", usuario));
-                    String sumaFallido = "UPDATE NET_A_CERO.Usuarios SET usr_intentos = usr_intentos + 1 WHERE usr_usuario = @username";
+                    String sumaFallido = "UPDATE PUSH_IT_TO_THE_LIMIT.Usuario SET usuario_intentos = usuario_intentos + 1 WHERE usuario_name = @username";
                     QueryBuilder.Instance.build(sumaFallido, parametros).ExecuteNonQuery();
 
 
                     // Si es el tercer fallido se deshabilita al usuario
                     parametros.Clear();
                     parametros.Add(new SqlParameter("@username", usuario));
-                    String cantidadFallidos = "SELECT usr_intentos FROM NET_A_CERO.Usuarios WHERE usr_usuario = @username";
+                    String cantidadFallidos = "SELECT usuario_intentos FROM PUSH_IT_TO_THE_LIMIT.Usuario WHERE usuario_name = @username";
                     int intentosFallidos = Convert.ToInt32(QueryBuilder.Instance.build(cantidadFallidos, parametros).ExecuteScalar());
 
                     if (intentosFallidos == 3)
                     {
                         parametros.Clear();
                         parametros.Add(new SqlParameter("@username", usuario));
-                        String deshabilitar = "UPDATE NET_A_CERO.Usuarios SET usr_activo = 0 WHERE usr_usuario = @username";
+                        String deshabilitar = "UPDATE PUSH_IT_TO_THE_LIMIT.Usuario SET usuario_habilitado = 0 WHERE usuario_name = @username";
                         QueryBuilder.Instance.build(deshabilitar, parametros).ExecuteNonQuery();
                     }
                     MessageBox.Show("Contraseña incorrecta." + '\n' + '\n' + "La contraseña distingue mayusculas y minusculas." + '\n' + '\n' + "Fallidos del usuario: " + intentosFallidos);
