1f9a287d0f055e676402dda1848e8479df94e859
Nico <nico_nobile@hotmail.com>
2017-06-14 15:52:29 -0300

    Diseño de registro de viajes

---
+++ src/UberFrba/Registro Viajes/AgregarViaje.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace UberFrba.Registro_Viajes
+{
+    partial class AgregarViaje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_datosViaje = new System.Windows.Forms.GroupBox();
+            this.label_Chofer = new System.Windows.Forms.Label();
+            this.textBox_Chofer = new System.Windows.Forms.TextBox();
+            this.label_Automovil = new System.Windows.Forms.Label();
+            this.textBox_Automovil = new System.Windows.Forms.TextBox();
+            this.textBox_Turno = new System.Windows.Forms.TextBox();
+            this.label_Turno = new System.Windows.Forms.Label();
+            this.label_CantidadKm = new System.Windows.Forms.Label();
+            this.textBox_CantidadKm = new System.Windows.Forms.TextBox();
+            this.label_FHfin = new System.Windows.Forms.Label();
+            this.label_FHinicio = new System.Windows.Forms.Label();
+            this.textBox_FHinicio = new System.Windows.Forms.TextBox();
+            this.textBox_FHfin = new System.Windows.Forms.TextBox();
+            this.label_Cliente = new System.Windows.Forms.Label();
+            this.textBox_Cliente = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button_FechaDeNacimiento = new System.Windows.Forms.Button();
+            this.button_Cancelar = new System.Windows.Forms.Button();
+            this.button_Limpiar = new System.Windows.Forms.Button();
+            this.button_Guardar = new System.Windows.Forms.Button();
+            this.groupBox_datosViaje.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_datosViaje
+            // 
+            this.groupBox_datosViaje.Controls.Add(this.button_FechaDeNacimiento);
+            this.groupBox_datosViaje.Controls.Add(this.textBox_Cliente);
+            this.groupBox_datosViaje.Controls.Add(this.label_Cliente);
+            this.groupBox_datosViaje.Controls.Add(this.textBox_FHfin);
+            this.groupBox_datosViaje.Controls.Add(this.textBox_FHinicio);
+            this.groupBox_datosViaje.Controls.Add(this.label_FHinicio);
+            this.groupBox_datosViaje.Controls.Add(this.label_FHfin);
+            this.groupBox_datosViaje.Controls.Add(this.textBox_CantidadKm);
+            this.groupBox_datosViaje.Controls.Add(this.label_CantidadKm);
+            this.groupBox_datosViaje.Controls.Add(this.label_Turno);
+            this.groupBox_datosViaje.Controls.Add(this.textBox_Turno);
+            this.groupBox_datosViaje.Controls.Add(this.textBox_Automovil);
+            this.groupBox_datosViaje.Controls.Add(this.label_Automovil);
+            this.groupBox_datosViaje.Controls.Add(this.textBox_Chofer);
+            this.groupBox_datosViaje.Controls.Add(this.label_Chofer);
+            this.groupBox_datosViaje.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_datosViaje.Name = "groupBox_datosViaje";
+            this.groupBox_datosViaje.Size = new System.Drawing.Size(437, 244);
+            this.groupBox_datosViaje.TabIndex = 0;
+            this.groupBox_datosViaje.TabStop = false;
+            this.groupBox_datosViaje.Text = "Datos del Viaje";
+            // 
+            // label_Chofer
+            // 
+            this.label_Chofer.AutoSize = true;
+            this.label_Chofer.Location = new System.Drawing.Point(6, 27);
+            this.label_Chofer.Name = "label_Chofer";
+            this.label_Chofer.Size = new System.Drawing.Size(66, 13);
+            this.label_Chofer.TabIndex = 22;
+            this.label_Chofer.Text = "Chofer (DNI)";
+            // 
+            // textBox_Chofer
+            // 
+            this.textBox_Chofer.Location = new System.Drawing.Point(158, 27);
+            this.textBox_Chofer.Name = "textBox_Chofer";
+            this.textBox_Chofer.Size = new System.Drawing.Size(188, 20);
+            this.textBox_Chofer.TabIndex = 23;
+            // 
+            // label_Automovil
+            // 
+            this.label_Automovil.AutoSize = true;
+            this.label_Automovil.Location = new System.Drawing.Point(6, 57);
+            this.label_Automovil.Name = "label_Automovil";
+            this.label_Automovil.Size = new System.Drawing.Size(112, 13);
+            this.label_Automovil.TabIndex = 24;
+            this.label_Automovil.Text = "Automovil (PATENTE)";
+            // 
+            // textBox_Automovil
+            // 
+            this.textBox_Automovil.Location = new System.Drawing.Point(158, 57);
+            this.textBox_Automovil.Name = "textBox_Automovil";
+            this.textBox_Automovil.Size = new System.Drawing.Size(273, 20);
+            this.textBox_Automovil.TabIndex = 25;
+            // 
+            // textBox_Turno
+            // 
+            this.textBox_Turno.Location = new System.Drawing.Point(158, 86);
+            this.textBox_Turno.Name = "textBox_Turno";
+            this.textBox_Turno.Size = new System.Drawing.Size(273, 20);
+            this.textBox_Turno.TabIndex = 26;
+            // 
+            // label_Turno
+            // 
+            this.label_Turno.AutoSize = true;
+            this.label_Turno.Location = new System.Drawing.Point(6, 86);
+            this.label_Turno.Name = "label_Turno";
+            this.label_Turno.Size = new System.Drawing.Size(35, 13);
+            this.label_Turno.TabIndex = 27;
+            this.label_Turno.Text = "Turno";
+            // 
+            // label_CantidadKm
+            // 
+            this.label_CantidadKm.AutoSize = true;
+            this.label_CantidadKm.Location = new System.Drawing.Point(6, 116);
+            this.label_CantidadKm.Name = "label_CantidadKm";
+            this.label_CantidadKm.Size = new System.Drawing.Size(115, 13);
+            this.label_CantidadKm.TabIndex = 28;
+            this.label_CantidadKm.Text = "Cantidad de Kilometros";
+            // 
+            // textBox_CantidadKm
+            // 
+            this.textBox_CantidadKm.Location = new System.Drawing.Point(158, 116);
+            this.textBox_CantidadKm.Name = "textBox_CantidadKm";
+            this.textBox_CantidadKm.Size = new System.Drawing.Size(273, 20);
+            this.textBox_CantidadKm.TabIndex = 29;
+            // 
+            // label_FHfin
+            // 
+            this.label_FHfin.AutoSize = true;
+            this.label_FHfin.Location = new System.Drawing.Point(6, 179);
+            this.label_FHfin.Name = "label_FHfin";
+            this.label_FHfin.Size = new System.Drawing.Size(125, 13);
+            this.label_FHfin.TabIndex = 30;
+            this.label_FHfin.Text = "Fecha y hora fin del viaje";
+            // 
+            // label_FHinicio
+            // 
+            this.label_FHinicio.AutoSize = true;
+            this.label_FHinicio.Location = new System.Drawing.Point(6, 145);
+            this.label_FHinicio.Name = "label_FHinicio";
+            this.label_FHinicio.Size = new System.Drawing.Size(138, 13);
+            this.label_FHinicio.TabIndex = 31;
+            this.label_FHinicio.Text = "Fecha y hora inicio del viaje";
+            // 
+            // textBox_FHinicio
+            // 
+            this.textBox_FHinicio.Location = new System.Drawing.Point(158, 145);
+            this.textBox_FHinicio.Name = "textBox_FHinicio";
+            this.textBox_FHinicio.Size = new System.Drawing.Size(273, 20);
+            this.textBox_FHinicio.TabIndex = 32;
+            // 
+            // textBox_FHfin
+            // 
+            this.textBox_FHfin.Location = new System.Drawing.Point(158, 179);
+            this.textBox_FHfin.Name = "textBox_FHfin";
+            this.textBox_FHfin.Size = new System.Drawing.Size(273, 20);
+            this.textBox_FHfin.TabIndex = 33;
+            // 
+            // label_Cliente
+            // 
+            this.label_Cliente.AutoSize = true;
+            this.label_Cliente.Location = new System.Drawing.Point(6, 209);
+            this.label_Cliente.Name = "label_Cliente";
+            this.label_Cliente.Size = new System.Drawing.Size(67, 13);
+            this.label_Cliente.TabIndex = 34;
+            this.label_Cliente.Text = "Cliente (DNI)";
+            // 
+            // textBox_Cliente
+            // 
+            this.textBox_Cliente.Location = new System.Drawing.Point(158, 209);
+            this.textBox_Cliente.Name = "textBox_Cliente";
+            this.textBox_Cliente.Size = new System.Drawing.Size(273, 20);
+            this.textBox_Cliente.TabIndex = 35;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(18, 271);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(174, 13);
+            this.label14.TabIndex = 36;
+            this.label14.Text = "* Debe completar todos los campos";
+            // 
+            // button_FechaDeNacimiento
+            // 
+            this.button_FechaDeNacimiento.Location = new System.Drawing.Point(351, 26);
+            this.button_FechaDeNacimiento.Name = "button_FechaDeNacimiento";
+            this.button_FechaDeNacimiento.Size = new System.Drawing.Size(80, 20);
+            this.button_FechaDeNacimiento.TabIndex = 36;
+            this.button_FechaDeNacimiento.Text = "Seleccionar";
+            this.button_FechaDeNacimiento.UseVisualStyleBackColor = true;
+            // 
+            // button_Cancelar
+            // 
+            this.button_Cancelar.Location = new System.Drawing.Point(21, 288);
+            this.button_Cancelar.Name = "button_Cancelar";
+            this.button_Cancelar.Size = new System.Drawing.Size(100, 30);
+            this.button_Cancelar.TabIndex = 37;
+            this.button_Cancelar.Text = "Volver";
+            this.button_Cancelar.UseVisualStyleBackColor = true;
+            this.button_Cancelar.Click += new System.EventHandler(this.button_Cancelar_Click);
+            // 
+            // button_Limpiar
+            // 
+            this.button_Limpiar.Location = new System.Drawing.Point(170, 288);
+            this.button_Limpiar.Name = "button_Limpiar";
+            this.button_Limpiar.Size = new System.Drawing.Size(100, 30);
+            this.button_Limpiar.TabIndex = 38;
+            this.button_Limpiar.Text = "Limpiar";
+            this.button_Limpiar.UseVisualStyleBackColor = true;
+            this.button_Limpiar.Click += new System.EventHandler(this.button_Limpiar_Click);
+            // 
+            // button_Guardar
+            // 
+            this.button_Guardar.Location = new System.Drawing.Point(323, 288);
+            this.button_Guardar.Name = "button_Guardar";
+            this.button_Guardar.Size = new System.Drawing.Size(100, 30);
+            this.button_Guardar.TabIndex = 39;
+            this.button_Guardar.Text = "Guardar";
+            this.button_Guardar.UseVisualStyleBackColor = true;
+            // 
+            // AgregarViaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 330);
+            this.Controls.Add(this.button_Guardar);
+            this.Controls.Add(this.button_Limpiar);
+            this.Controls.Add(this.button_Cancelar);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.groupBox_datosViaje);
+            this.Name = "AgregarViaje";
+            this.Text = "AgregarViaje";
+            this.groupBox_datosViaje.ResumeLayout(false);
+            this.groupBox_datosViaje.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_datosViaje;
+        private System.Windows.Forms.Label label_Chofer;
+        private System.Windows.Forms.TextBox textBox_Chofer;
+        private System.Windows.Forms.Label label_Automovil;
+        private System.Windows.Forms.Label label_Turno;
+        private System.Windows.Forms.TextBox textBox_Turno;
+        private System.Windows.Forms.TextBox textBox_Automovil;
+        private System.Windows.Forms.Label label_CantidadKm;
+        private System.Windows.Forms.TextBox textBox_CantidadKm;
+        private System.Windows.Forms.TextBox textBox_Cliente;
+        private System.Windows.Forms.Label label_Cliente;
+        private System.Windows.Forms.TextBox textBox_FHfin;
+        private System.Windows.Forms.TextBox textBox_FHinicio;
+        private System.Windows.Forms.Label label_FHinicio;
+        private System.Windows.Forms.Label label_FHfin;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button_FechaDeNacimiento;
+        private System.Windows.Forms.Button button_Cancelar;
+        private System.Windows.Forms.Button button_Limpiar;
+        private System.Windows.Forms.Button button_Guardar;
+    }
+}
---
+++ src/UberFrba/Registro Viajes/AgregarViaje.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UberFrba.Registro_Viajes
+{
+    public partial class AgregarViaje : Form
+    {
+        public AgregarViaje()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Cancelar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new MenuPrincipal().ShowDialog();
+            this.Close();
+        }
+
+        private void button_Limpiar_Click(object sender, EventArgs e)
+        {
+            textBox_Automovil.Text = "";
+            textBox_CantidadKm.Text = "";
+            textBox_Chofer.Text = "";
+            textBox_Cliente.Text = "";
+            textBox_FHfin.Text = "";
+            textBox_FHinicio = "";
+            textBox_Turno = "";
+        }
+    }
+}
