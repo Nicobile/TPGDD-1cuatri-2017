e82498db640d5f46c41ce39ccdbef37bbc031306
Brian Ezeiza <brianezeiza@hotmail.com>
2017-07-01 02:55:06 -0300

    Se marcan correctamente los checbox de roles en asignar roles a usuario

--- src/UberFrba/ABM Rol/AsignarRolesUsuario.cs
+++ src/UberFrba/ABM Rol/AsignarRolesUsuario.cs
@@ -53,154 +53,73 @@ namespace UberFrba.Abm_Rol
             checkedListBoxFuncionalidades.ValueMember = "rol_nombre";
         }
 
-        
+
 
         private void comboBox_Usuarios_SelectedIndexChanged(object sender, EventArgs e)
         {
-                int idUsuario = Convert.ToInt32(mapper.SelectFromWhere("usuario_id", "Usuario", "usuario_name", comboBox_Usuarios.Text));
-                List<SqlParameter> parametros = new List<SqlParameter>();
-                parametros.Clear();
-                parametros.Add(new SqlParameter("@idUsuario", idUsuario));
-                SqlCommand command= QueryBuilder.Instance.build("select rol_nombre from PUSH_IT_TO_THE_LIMIT.rol r join PUSH_IT_TO_THE_LIMIT.RolporUsuario p on(r.rol_id=p.rol_id) join PUSH_IT_TO_THE_LIMIT.Usuario u ON(p.usuario_id = u.usuario_id) where u.usuario_id=@idUsuario AND r.rol_estado=1", parametros);        
-              //  command.Parameters.AddWithValue("@idUsuario", idUsuario);
-                int i = 0;
-                while (i++<checkedListBoxFuncionalidades.Items.Count) {
-                    checkedListBoxFuncionalidades.SetItemChecked(i-1, false);               
-                }
-                actualizarRoles(command);                       
+            MarcarFuncionalidades();
         }
         
-        // private void actualizarRoles (SqlCommand command) {                 
-        //    SqlDataReader roles = command.ExecuteReader();
-        //    while (roles.Read()) {
-        //        string rol = (string)roles["rol_nombre"];
-        //        int i = 0;
-        //        bool encontrado=false;
-        //        while (!encontrado) {
-        //            if (checkedListBoxFuncionalidades.Va == rol) {
-        //                checkedListBoxFuncionalidades.SetItemChecked(i, true);
-        //                encontrado=true;
-        //            }
-        //            i++;
-        //        }
-        //    }
-        //    roles.Close();
-        //}
+        
 
-         private void actualizarRoles(SqlCommand command)
+         private void MarcarFuncionalidades()
          {
-             SqlDataReader roles = command.ExecuteReader();
-             while (roles.Read())
+             int idUsuario = Convert.ToInt32(mapper.SelectFromWhere("usuario_id", "Usuario", "usuario_name", comboBox_Usuarios.Text));
+             IList<SqlParameter> parametros = new List<SqlParameter>();
+             SqlDataAdapter adapter = new SqlDataAdapter();
+             List<int> funcionalidadesAMarcar = new List<int>();
+             DesmarcarFuncionalidades();
+
+             foreach (DataRowView funcionalidad in this.checkedListBoxFuncionalidades.Items)
              {
-                 string rol = (string)roles["rol_nombre"];
+                 parametros.Clear();
+                 parametros.Add(new SqlParameter("@funcionalidad", funcionalidad.Row["rol_nombre"] as String));
 
-                 for (int i = 0; i < checkedListBoxFuncionalidades.Items.Count; i++)
+                 if (verificarSiLaTiene(funcionalidad.Row["rol_nombre"] as String,idUsuario))
                  {
-
-
-                     if (checkedListBoxFuncionalidades.Items[i].ToString() == rol)
-                     {
-                         checkedListBoxFuncionalidades.SetItemChecked(i, true);
-
-                     }
-                     else { checkedListBoxFuncionalidades.SetItemChecked(i, false); }
-
+                     int i = checkedListBoxFuncionalidades.Items.IndexOf(funcionalidad);
+                     funcionalidadesAMarcar.Add(i);
 
                  }
-                 
              }
-             roles.Close();
-         }         
-        
-        
-        
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //private void comboBox_Usuarios_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-        //    int i = 0;
-        //    while (i++ < checkedListBoxFuncionalidades.Items.Count)
-        //    {
-        //        checkedListBoxFuncionalidades.SetItemChecked(i - 1, false);               //Desactivo todos los roles
-        //    }
-        //    int idUsuario = Convert.ToInt32(mapper.SelectFromWhere("usuario_id", "Usuario", "usuario_name", comboBox_Usuarios.Text));
-        //    DataTable rolesUsuario = mapper.obtenerRolesDeUnUsuario(idUsuario);
-        //    actualizarRoles(rolesUsuario);                       //Activo los del usuario
-
-        //}
-        //private void actualizarRoles(DataTable rolesUser)
-        //{
-        //    CargarFuncionalidades();
-
-        //    foreach (DataRow fila in rolesUser.Rows)
-        //    {
-        //        string rolNombre = fila["rol_nombre"].ToString();
-
-        //        int i = 0;
-        //        while ((checkedListBoxFuncionalidades.Items.Count) >= i)
-        //        {
-        //            bool encontrado = false;
-        //            while (!encontrado)
-        //            {
-        //                String rolList = Convert.ToString(checkedListBoxFuncionalidades.Items[i]);
-        //                // String chupala = convert.checkedListBoxFuncionalidades.SelectedItem;
-        //                checkedListBoxFuncionalidades.DisplayMember = "NameAndScore";
-        //                checkedListBoxFuncionalidades.ValueMember = "NameAndScore";
-        //                //checkedListBoxFuncionalidades.DataSource = dSet.Tables[0];
-
-        //                if (rolList == "Administrador")
-        //                {
-
-        //                    checkedListBoxFuncionalidades.SetItemChecked(i, true);
-        //                    encontrado = true;
-        //                }
-        //                i++;
-        //            }
-        //        }
-
-        //        lstNames.DisplayMember = "NameAndScore"; lstNames.ValueMember = "NameAndScore"; lstNames.DataSource = dSet.Tables[0];
+             foreach (int index in funcionalidadesAMarcar)
+             {
+                 checkedListBoxFuncionalidades.SetItemChecked(index, true);
+             }
+         }
 
 
 
-        //    }
-
-    //    }
-
-
-
-
-
-
+         private void DesmarcarFuncionalidades()
+         {
+             for (int i = 0; i < checkedListBoxFuncionalidades.Items.Count; i++)
+             {
+                 checkedListBoxFuncionalidades.SetItemChecked(i, false);
+             }
+         }
 
 
 
+         private bool verificarSiLaTiene(String funcionalidad,int IDUsuario)
+         {
+             IList<SqlParameter> parametros = new List<SqlParameter>();
+             parametros.Clear();
+             parametros.Add(new SqlParameter("@idUsuario", IDUsuario));
+             parametros.Add(new SqlParameter("@funcionalidad", funcionalidad));
 
+             String queryCantidadRolXFuncionalidad = "SELECT COUNT(*) from PUSH_IT_TO_THE_LIMIT.rol r join PUSH_IT_TO_THE_LIMIT.RolporUsuario p on(r.rol_id=p.rol_id) join PUSH_IT_TO_THE_LIMIT.Usuario u ON(p.usuario_id = u.usuario_id) where u.usuario_id=@idUsuario and r.rol_nombre=@funcionalidad";
+             int tieneLaFuncionalidad = (int)QueryBuilder.Instance.build(queryCantidadRolXFuncionalidad, parametros).ExecuteScalar();
 
-
-
-
-
-
-
-
-
-
-
-
+             if (tieneLaFuncionalidad == 1)
+             {
+                 return true;
+             }
+             else
+             {
+                 return false;
+             }
+         }
 
         
     }
